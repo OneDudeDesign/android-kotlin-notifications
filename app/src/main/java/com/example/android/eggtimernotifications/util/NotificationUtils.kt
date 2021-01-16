80d66aa1df4ee6c718383e1aea54d2367e17a7a6
Christopher Lucier <OneDudeDesign@gmail.com>
2021-01-16 10:42:44 -0800

    added bigpicture style to show large egg when notification is expanded

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -51,7 +51,15 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         PendingIntent.FLAG_UPDATE_CURRENT
     )
 
-    // TODO: Step 2.0 add style
+    // DONE: Step 2.0 add style
+    val eggImage = BitmapFactory.decodeResource(
+        applicationContext.resources,
+        R.drawable.cooked_egg
+    )
+
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(eggImage)
+        .bigLargeIcon(null)
 
     // TODO: Step 2.2 add snooze action
 
@@ -66,6 +74,8 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setContentText(messageBody)
         .setContentIntent(contentPendingIntent)
         .setAutoCancel(true)
+        .setStyle(bigPicStyle)
+        .setLargeIcon(eggImage)
 
     // TODO: Step 1.8 use the new 'breakfast' notification channel
 
@@ -73,7 +83,7 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 
     // DONE: Step 1.13 set content intent
 
-    // TODO: Step 2.1 add style to builder
+    // DONE: Step 2.1 add style to builder
 
     // TODO: Step 2.3 add snooze action
 
